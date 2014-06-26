7bed0765b33345e47771195b13e7e06c76610281
Vinayakumar B <vinayakumar.b@intel.com>
2014-06-26 09:50:50 +0530

    added index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
  * This is the starting file for the project.
  */
 
-//define('DS', DIRECTORY_SEPARATOR);
+define('DS', DIRECTORY_SEPARATOR);
 //Installation root directory
-//define('INSTALL_ROOT_DIR', dirname(__FILE__).DS);
+define('INSTALL_ROOT_DIR', dirname(__FILE__).DS);
 
 //required directories
-//define('CONFIG_DIR', INSTALL_ROOT_DIR . 'config' . DS);
+define('CONFIG_DIR', INSTALL_ROOT_DIR . 'config' . DS);
 
 ?>
