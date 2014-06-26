c91324d063c94d836fe277f887bdb48093d87412
Vinayakumar B <vinayakumar.b@intel.com>
2014-06-26 09:44:08 +0530

    Adding the project settings

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// index.php
+/*
+ * This is the starting file for the project.
+ */
+
+//define('DS', DIRECTORY_SEPARATOR);
+//Installation root directory
+//define('INSTALL_ROOT_DIR', dirname(__FILE__).DS);
+
+//required directories
+//define('CONFIG_DIR', INSTALL_ROOT_DIR . 'config' . DS);
+
+?>
